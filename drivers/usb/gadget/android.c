4601b0516f5d6f99b648a03e235f4efe6f42f203
= <newhor1z00n@gmail.com>
2014-08-15 08:28:17 +0530

    fixed typo

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -2577,7 +2577,7 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 					pr_err("android_usb: Cannot enable %s",
 						name);
 				if (!strcmp(name, "hid"))
-+					hid_enabled = 1;
+					hid_enabled = 1;
 			}
 		}
 		/* HID driver always enabled, it's the whole point of this kernel patch */
